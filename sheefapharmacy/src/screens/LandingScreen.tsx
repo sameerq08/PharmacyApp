e71e4de12568a7bbdfcf91ba4c5c2c1e450d59f7
unknown <ZII5021@ZII5021.ziic.net>
2023-01-01 15:34:53 +0530

    Screen added

---
+++ sheefapharmacy/src/screens/LandingScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet, Dimensions, Image} from 'react-native'
+
+export const LandingScreen = {} => {
+    return {
+        <View>
+            <Text></Text>
+        </View>
+    }
+}
