f6ad05274ba9aa0a8581771ba976ba0cba7aca58
unknown <ZII5021@ZII5021.ziic.net>
2023-01-01 20:06:52 +0530

    new dolfer added

--- sheefapharmacy/src/screens/LandingScreen.tsx
+++ sheefapharmacy/src/screens/LandingScreen.tsx
@@ -1,10 +1,3 @@
 import React from 'react'
 import { View, Text, StyleSheet, Dimensions, Image} from 'react-native'
 
-export const LandingScreen = {} => {
-    return {
-        <View>
-            <Text></Text>
-        </View>
-    }
-}
